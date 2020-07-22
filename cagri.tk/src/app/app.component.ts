2d5d80854562d022803a98df700fc21ed7937e08
cagritrk <haliscagri@gmail.com>
2020-07-23 02:43:26 +0300

    Updating Pages

--- cagri.tk/src/app/app.component.ts
+++ cagri.tk/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testAngularApp';
+  menuItems: any[];
+  activePage: String = 'Home';
+
+  constructor(){
+    this.menuItems = [
+      {class: 'nav-link active', href: '#', name: 'Home', id: 0},
+      {class: 'nav-link', href: 'https://github.com/cagritrk', name: 'GitHub', target: "_blank", id: 1},
+      {class: 'nav-link', href: '#', name: 'TodoList', id: 2},
+      {class: 'nav-link', href: '#', name: 'About', id: 3},
+      {class: 'nav-link', href: '#', name: 'Contact', id: 4},
+    ];
+  }
+
+  setActivePage (name:String) {
+    this.activePage = name;
+    console.log("Active page is :" + this.activePage);
+  }
 }
--- cagri.tk/src/app/app.module.ts
+++ cagri.tk/src/app/app.module.ts
@@ -17,4 +17,4 @@ import { HomeComponent } from './home/home.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
